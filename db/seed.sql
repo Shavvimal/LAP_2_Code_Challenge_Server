e9c2b731e1e94dd332c393d28aa34a839718dd7c
moony98 <84647389+moony98@users.noreply.github.com>
2021-07-21 13:05:30 +0100

    server side

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO posts (title, name, content)
+    VALUES
+    ('Monday', 'Shav', 'Did an exam and it went well :)')
+    ('Tuesday', 'Moony', 'Did a team challenge and got stuck, oops :(')
+)
